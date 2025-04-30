9c3b72f23a1f9c6834327e56b25d836fb3c3d3a1
ngocson07042004 <tranthai16102004@gmail.com>
2025-04-30 17:33:48 +0700

    up code

--- frontend/src/pages-client/index.js
+++ frontend/src/pages-client/index.js
@@ -2,6 +2,7 @@ import Home from './Home'
 import Introduction from './Introduction'
 import Product from './Product'
 import Question from './Question'
+import Recruitment from './Recruitment'
 import Contact from './Contact'
 
-export { Home, Introduction, Product, Question, Contact }
+export { Home, Introduction, Product, Recruitment, Question, Contact }
